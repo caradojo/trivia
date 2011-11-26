0aea0c35da61376a02645fbb1855bd8b2b0c4f9a
J. B. Rainsberger <me@jbrains.ca>
2011-11-26 12:32:12 +0100

    Fixed a porting mistake into Ruby that created an unfair bug in the Ruby version.

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -5,15 +5,15 @@ module UglyTrivia
       @places = Array.new(6, 0)
       @purses = Array.new(6, 0)
       @in_penalty_box = Array.new(6, 0)
-      
+
       @pop_questions = []
       @science_questions = []
       @sports_questions = []
       @rock_questions = []
-      
+
       @current_player = 0
       @is_getting_out_of_penalty_box = false
-      
+
       50.times do |i|
         @pop_questions.push "Pop Question #{i}"
         @science_questions.push "Science Question #{i}"
@@ -21,43 +21,43 @@ module UglyTrivia
         @rock_questions.push create_rock_question(i)
       end
     end
-    
+
     def create_rock_question(index)
       "Rock Question #{index}"
     end
-    
+
     def is_playable?
       how_many_players >= 2
     end
-    
+
     def add(player_name)
       @players.push player_name
       @places[how_many_players] = 0
       @purses[how_many_players] = 0
       @in_penalty_box[how_many_players] = false
-      
+
       puts player_name + " was added"
       puts "They are player number #{@players.length}"
-      
+
       true
     end
-    
+
     def how_many_players
       @players.length
     end
-    
+
     def roll(roll)
       puts "#{@players[@current_player]} is the current player"
       puts "They have rolled a #{roll}"
-      
+
       if @in_penalty_box[@current_player]
         if roll % 2 != 0
-          is_getting_out_of_penalty_box = true
-          
+          @is_getting_out_of_penalty_box = true
+
           puts "#{@players[@current_player]} is getting out of the penalty box"
           @places[@current_player] = @places[@current_player] + roll
           @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
-          
+
           puts @players[@current_player] +
                 '\'s new location is' +
                 @places[@current_player].to_s
@@ -65,14 +65,14 @@ module UglyTrivia
           ask_question
         else
           puts "#{@players[@current_player]} is not getting out of the penalty box"
-          is_getting_out_of_penalty_box = false
+          @is_getting_out_of_penalty_box = false
           end
-          
+
       else
-        
+
         @places[@current_player] = @places[@current_player] + roll
         @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
-        
+
         puts @players[@current_player] +
               '\'s new location is' +
               @places[@current_player].to_s
@@ -80,16 +80,16 @@ module UglyTrivia
         ask_question
       end
     end
-    
+
   private
-    
+
     def ask_question
       puts @pop_questions.shift if current_category == 'Pop'
       puts @science_questions.shift if current_category == 'Science'
       puts @sports_questions.shift if current_category == 'Sports'
       puts @rock_questions.shift if current_category == 'Rock'
     end
-    
+
     def current_category
       return 'Pop' if @places[@current_player] == 0
       return 'Pop' if @places[@current_player] == 4
@@ -102,9 +102,9 @@ module UglyTrivia
       return 'Sports' if @places[@current_player] == 10
       return 'Rock'
     end
-  
+
   public
-  
+
     def was_correctly_answered
       if @in_penalty_box[@current_player]
         if @is_getting_out_of_penalty_box
@@ -114,37 +114,37 @@ module UglyTrivia
             ' now has ' +
             @purses[@current_player] +
             ' Gold Coins.'
-          
+
           winner = did_player_win()
           @current_player += 1
           @current_player = 0 if @current_player == @players.length
-          
+
           winner
         else
           @current_player += 1
           @current_player = 0 if @current_player == @players.length
           true
         end
-        
-        
-        
+
+
+
       else
-        
+
         puts "Answer was corrent!!!!"
         @purses[@current_player] += 1
         puts @players[@current_player] +
           ' now has ' +
           @purses[@current_player].to_s +
           ' Gold Coins.'
-        
+
         winner = did_player_win
         @current_player += 1
         @current_player = 0 if @current_player == @players.length
-        
+
         return winner
       end
     end
-    
+
     def wrong_answer
   		puts 'Question was incorrectly answered'
   		puts @players[@current_player] + " was sent to the penalty box"
@@ -154,9 +154,9 @@ module UglyTrivia
       @current_player = 0 if @current_player == @players.length
   		return true
     end
-    
+
   private
-  
+
     def did_player_win
       !(@purses[@current_player] == 6)
     end
