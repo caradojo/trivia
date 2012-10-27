1a10b25602fa2dcd7d631524d38f663cec40af25
Jorge Lee <jrg.lee@gmail.com>
2012-10-27 19:45:50 -0400

    Export Game function for testing.

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -1,5 +1,15 @@
+require('./game.js');
+
 describe("The test environment", function() {
   it("should pass", function() {
     expect(true).toBe(true);
   });
-})
+
+  it("should access game", function() {
+    expect(Game).toBeDefined();
+  });
+});
+
+describe("Your specs...", function() {
+  // it ...
+});
