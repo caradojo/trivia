87e239c48200bede0346fc0ab2357fe52307c39b
Remy Sanlaville <remy.sanlaville@gmail.com>
2014-03-07 13:04:33 +0100

    fix GameTest

--- src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,40 +1,35 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-
 
 public class GameRunner {
 
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
-	    Random rand = new Random();
+		Random rand = new Random();
 		playGame(rand);
-		
 	}
 
-    public static void playGame(Random rand) {
+	public static void playGame(Random randomizer) {
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
+
+			aGame.roll(randomizer.nextInt(5) + 1);
+
+			if (randomizer.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
 		} while (notAWinner);
-		
 	}
 }
