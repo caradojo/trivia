b7cab9372e937661910649b2f0c878e69bb9249b
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-03-16 17:02:59 +0100

    Golden Master with ApprovalTests

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -14,32 +14,36 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
-            Game aGame = new Game();
-
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
-
-            Random rand = new Random();
-
-            do
+            for (int i = 0; i < 10; i++)
             {
 
-                aGame.roll(rand.Next(5) + 1);
+                Game aGame = new Game();
 
-                if (rand.Next(9) == 7)
+                aGame.add("Chet");
+                aGame.add("Pat");
+                aGame.add("Sue");
+
+                Random rand = new Random(i);
+
+                do
                 {
-                    notAWinner = aGame.wrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.wasCorrectlyAnswered();
-                }
+
+                    aGame.roll(rand.Next(5) + 1);
+
+                    if (rand.Next(9) == 7)
+                    {
+                        notAWinner = aGame.wrongAnswer();
+                    }
+                    else
+                    {
+                        notAWinner = aGame.wasCorrectlyAnswered();
+                    }
 
 
 
-            } while (notAWinner);
+                } while (notAWinner);
 
+            }
         }
 
 
