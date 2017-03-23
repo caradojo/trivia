78cea8b35c9a7986f4a558251efe9cee71fd6a9b
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-03-23 14:30:59 +0100

    Add player class with Name property

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -8,7 +8,7 @@ namespace Trivia
     {
 
 
-        List<string> players = new List<string>();
+        List<Player> players = new List<Player>();
 
         int[] places = new int[6];
         int[] purses = new int[6];
@@ -44,11 +44,9 @@ namespace Trivia
             return (HowManyPlayers() >= 2);
         }
 
-        public bool Add(String playerName)
+        public bool Add(string playerName)
         {
-
-
-            players.Add(playerName);
+            players.Add(new Player(playerName));
             places[HowManyPlayers()] = 0;
             purses[HowManyPlayers()] = 0;
             inPenaltyBox[HowManyPlayers()] = false;
@@ -65,7 +63,7 @@ namespace Trivia
 
         public void Roll(int roll)
         {
-            Console.WriteLine(players[currentPlayer] + " is the current player");
+            Console.WriteLine(players[currentPlayer].Name + " is the current player");
             Console.WriteLine("They have rolled a " + roll);
 
             if (inPenaltyBox[currentPlayer])
@@ -74,11 +72,11 @@ namespace Trivia
                 {
                     isGettingOutOfPenaltyBox = true;
 
-                    Console.WriteLine(players[currentPlayer] + " is getting out of the penalty box");
+                    Console.WriteLine(players[currentPlayer].Name + " is getting out of the penalty box");
                     places[currentPlayer] = places[currentPlayer] + roll;
                     if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
-                    Console.WriteLine(players[currentPlayer]
+                    Console.WriteLine(players[currentPlayer].Name
                             + "'s new location is "
                             + places[currentPlayer]);
                     Console.WriteLine("The category is " + CurrentCategory());
@@ -86,7 +84,7 @@ namespace Trivia
                 }
                 else
                 {
-                    Console.WriteLine(players[currentPlayer] + " is not getting out of the penalty box");
+                    Console.WriteLine(players[currentPlayer].Name + " is not getting out of the penalty box");
                     isGettingOutOfPenaltyBox = false;
                 }
 
@@ -97,7 +95,7 @@ namespace Trivia
                 places[currentPlayer] = places[currentPlayer] + roll;
                 if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
-                Console.WriteLine(players[currentPlayer]
+                Console.WriteLine(players[currentPlayer].Name
                         + "'s new location is "
                         + places[currentPlayer]);
                 Console.WriteLine("The category is " + CurrentCategory());
@@ -153,7 +151,7 @@ namespace Trivia
                 {
                     Console.WriteLine("Answer was correct!!!!");
                     purses[currentPlayer]++;
-                    Console.WriteLine(players[currentPlayer]
+                    Console.WriteLine(players[currentPlayer].Name
                             + " now has "
                             + purses[currentPlayer]
                             + " Gold Coins.");
@@ -179,7 +177,7 @@ namespace Trivia
 
                 Console.WriteLine("Answer was corrent!!!!");
                 purses[currentPlayer]++;
-                Console.WriteLine(players[currentPlayer]
+                Console.WriteLine(players[currentPlayer].Name
                         + " now has "
                         + purses[currentPlayer]
                         + " Gold Coins.");
@@ -195,7 +193,7 @@ namespace Trivia
         public bool WrongAnswer()
         {
             Console.WriteLine("Question was incorrectly answered");
-            Console.WriteLine(players[currentPlayer] + " was sent to the penalty box");
+            Console.WriteLine(players[currentPlayer].Name + " was sent to the penalty box");
             inPenaltyBox[currentPlayer] = true;
 
             currentPlayer++;
@@ -209,5 +207,4 @@ namespace Trivia
             return purses[currentPlayer] != 6;
         }
     }
-
 }
---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,12 @@
+namespace Trivia
+{
+    internal class Player
+    {
+        public string Name { get; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
